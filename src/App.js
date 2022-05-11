64eecab34a0913aadc01b07a4463b0a539b16950
PasqualCarstens <pasqualcarstens@gmail.com>
2022-05-11 22:30:39 +1000

    Created navigation bar and linked pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Slido from "./components/Slido";
+import Blanko from "./components/Blanko";
+import Tetro from "./components/Tetro";
 
-function App() {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="Body">
-      <Header />
-    </div>
+    <Router>
+      <div className="Body">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/slido" element={<Slido />} />
+          <Route path="/blanko" element={<Blanko />} />
+          <Route path="/tetro" element={<Tetro />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
-export default App;
