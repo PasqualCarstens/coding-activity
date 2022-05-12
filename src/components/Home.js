55daa277b4440f071e44af5f7609c6f983333247
PasqualCarstens <pasqualcarstens@gmail.com>
2022-05-12 10:12:52 +1000

    Added dashboard to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 
 export default function Home() {
+  const [score, setScore] = useState();
+
+  //Get Score on page load.
+  useEffect(() => {
+    fetchScoreData();
+  }, []);
+
+  async function fetchScoreData() {
+    let localScore = localStorage.getItem("score");
+    if (!localScore) {
+      let response = await fetch(
+        "https://cs6080.web.cse.unsw.edu.au/raw/data/info.json"
+      );
+      let data = await response.json();
+      console.log(data);
+      localStorage.setItem("score", data.score);
+      setScore(data.score);
+      return;
+    }
+    setScore(localScore);
+  }
+
+  const reset = () => {
+    localStorage.removeItem("score");
+    fetchScoreData();
+  };
+
   return (
     <div className="Home">
-      <h2 className="Text">Please choose an option from the navbar</h2>
-      <text>Games Won: X</text>
-      <button>Reset</button>
+      <div className="Text">Please choose an option from the Navbar.</div>{" "}
+      <br />
+      <div>Games Won: {score}</div>
+      <br />
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
