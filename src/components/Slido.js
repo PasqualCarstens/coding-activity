45d89ca45fc03aaad0ad374448fe5602b486513a
PasqualCarstens <pasqualcarstens@gmail.com>
2022-05-18 21:55:41 +1000

    Added shrek images to slido game

--- src/components/Slido.js
+++ src/components/Slido.js
@@ -1,5 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import one from "../data/shrek/one.png";
+import two from "../data/shrek/two.png";
+import three from "../data/shrek/three.png";
+import four from "../data/shrek/four.png";
+import five from "../data/shrek/five.png";
+import six from "../data/shrek/six.png";
+import seven from "../data/shrek/seven.png";
+import eight from "../data/shrek/eight.png";
 
 const Container = styled.div`
   width: 100%;
@@ -9,15 +17,17 @@ const Container = styled.div`
   justify-content: center;
 `;
 const Wrapper = styled.div`
-  width: 150px;
-  height: 150px;
+  width: 500px;
+  height: 500px;
   display: flex;
   flex-wrap: wrap;
   align-items: cenetr;
   justify-content: space-around;
 `;
 
-const Block = styled.div`
+const Block = styled.img`
+  width: 150px;
+  height: 150px;
   border: 1px solid #333;
   flex-basis: 32%;
   display: flex;
@@ -27,24 +37,22 @@ const Block = styled.div`
 
 export default function Slido() {
   let blocks = [
-    { id: 1, value: "A" },
-    { id: 2, value: "B" },
-    { id: 3, value: "C" },
-    { id: 4, value: "D" },
-    { id: 5, value: "E" },
-    { id: 6, value: "F" },
-    { id: 7, value: "G" },
-    { id: 8, value: "H" },
+    { id: 1, value: "A", image: one },
+    { id: 2, value: "B", image: two },
+    { id: 3, value: "C", image: three },
+    { id: 4, value: "D", image: four },
+    { id: 5, value: "E", image: five },
+    { id: 6, value: "F", image: six },
+    { id: 7, value: "G", image: seven },
+    { id: 8, value: "H", image: eight },
     { id: 9, value: "" },
   ];
 
   return (
     <Container>
       <Wrapper>
-        {blocks.map((block, index) => (
-          <Block key={block.id} tabIndex={index + 1} id={block.id}>
-            {block.value}
-          </Block>
+        {blocks.map((block) => (
+          <Block key={block.id} id={block.id} src={block.image}></Block>
         ))}
       </Wrapper>
     </Container>
